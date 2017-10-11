a42f1a3f1a7dc1d7a396e955ec2ab42f3cf636d2
MadCat <orloffski@gmail.com>
2017-10-12 01:58:07 +0300

    Rev 0.1.4 Add animations to menu buttons

--- src/view/RootController.java
+++ src/view/RootController.java
@@ -1,8 +1,10 @@
 package view;
 
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
 
 public class RootController {
 	
@@ -26,7 +28,6 @@ public class RootController {
 	
 	@FXML
 	private void initialize() {
-		
 	}
 	
 	@FXML
@@ -52,5 +53,88 @@ public class RootController {
 		ChartController controller = new ChartController();
 		rootLayout.setCenter(controller.getRootNode());
 	}
+	
+	private void biggerImage(ImageView image) {
+		final double SCALE = 1.3; // коэффициент увеличения
+	    final double DURATION = 300; // время анимации в мс
 
+	    // создаём анимацию увеличения картинки     
+	    final ScaleTransition animationGrow = 
+	    		new ScaleTransition(Duration.millis(DURATION), image);
+	    animationGrow.setToX(SCALE);
+	    animationGrow.setToY(SCALE);
+
+	    // и уменьшения
+	    final ScaleTransition animationShrink = 
+	    		new ScaleTransition(Duration.millis(DURATION), image);
+	    animationShrink.setToX(1);
+	    animationShrink.setToY(1);
+	    
+	    image.toFront();
+	    
+	    animationShrink.stop();
+        animationGrow.playFromStart();
+	}
+	
+	private void smallerImage(ImageView image) {
+		final double SCALE = 1.3; // коэффициент увеличения
+	    final double DURATION = 300; // время анимации в мс
+
+	    // создаём анимацию увеличения картинки     
+	    final ScaleTransition animationGrow = 
+	    		new ScaleTransition(Duration.millis(DURATION), image);
+	    animationGrow.setToX(SCALE);
+	    animationGrow.setToY(SCALE);
+
+	    // и уменьшения
+	    final ScaleTransition animationShrink = 
+	    		new ScaleTransition(Duration.millis(DURATION), image);
+	    animationShrink.setToX(1);
+	    animationShrink.setToY(1);
+	    
+	    animationGrow.stop();
+        animationShrink.playFromStart();
+	}
+	
+	@FXML
+	private void cashboxImageEntered() {
+		biggerImage(cashbox);
+	}
+	
+	@FXML
+	private void cashboxImageExited() {
+		smallerImage(cashbox);
+	}
+	
+	@FXML
+	private void chartImageEntered() {
+		biggerImage(chart);
+	}
+	
+	@FXML
+	private void chartImageExited() {
+		smallerImage(chart);
+	}
+	
+	@FXML
+	private void financeImageEntered() {
+		biggerImage(finance);
+	}
+	
+	@FXML
+	private void financeImageExited() {
+		smallerImage(finance);
+	}
+	
+	@FXML
+	private void stockImageEntered() {
+		biggerImage(stock);
+	}
+	
+	@FXML
+	private void stockImageExited() {
+		smallerImage(stock);
+	}
+	
+	
 }
