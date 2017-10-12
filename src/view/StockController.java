f20e2d0c0cb488e938efc7da3a3a6544c63d829f
MadCat <orloffski@gmail.com>
2017-10-13 00:54:16 +0300

    Rev 0.1.5 Add Stock subMenu, add Stock subViews

--- src/view/StockController.java
+++ src/view/StockController.java
@@ -5,9 +5,26 @@ import java.io.IOException;
 import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 public class StockController {
+	
+	@FXML
+    private BorderPane stockRootLayout;
+	
+	@FXML
+	private ImageView items;
+	
+	@FXML
+	private ImageView receipts;
+	
+	@FXML
+	private ImageView returns;
+	
+	@FXML
+	private ImageView transfers;
 	
 	public StockController() {
 	}
@@ -17,16 +34,40 @@ public class StockController {
 		
 	}
 	
+	@FXML
+	private void openItemsView(){
+        stockRootLayout.setCenter(getLoadedPane("/view/stockviews/ItemsView.fxml"));
+	}
+	
+	@FXML
+	private void openReceiptsView(){
+        stockRootLayout.setCenter(getLoadedPane("/view/stockviews/ReceiptsView.fxml"));
+	}
+	
+	@FXML
+	private void openReturnsView(){
+        stockRootLayout.setCenter(getLoadedPane("/view/stockviews/ReturnsView.fxml"));
+	}
+	
+	@FXML
+	private void openTransfersView(){
+        stockRootLayout.setCenter(getLoadedPane("/view/stockviews/TransfersView.fxml"));
+	}
+	
 	public AnchorPane getRootNode() {
-		AnchorPane rootView = null;
-		
+		return getLoadedPane("/view/StockView.fxml");
+	}
+	
+	public AnchorPane getLoadedPane(String viewLocation) {
+		AnchorPane paneView = null;
 		FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("/view/StockView.fxml"));
+        loader.setLocation(Main.class.getResource(viewLocation));
         try {
-        	rootView = (AnchorPane) loader.load();
+        	paneView = (AnchorPane) loader.load();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return rootView;
+        
+        return paneView;
 	}
 }
