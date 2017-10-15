13a8d371eaea9b6c4a5ad37ab6e0ac45d436fbe0
MadCat <orloffski@gmail.com>
2017-10-16 00:46:10 +0300

    Rev 0.1.12 add ico to add-edit item card, change left margin filter on itemTable

--- src/view/stockviews/ItemsViewController.java
+++ src/view/stockviews/ItemsViewController.java
@@ -20,6 +20,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
@@ -184,6 +185,7 @@ public class ItemsViewController {
 			AnchorPane page = (AnchorPane) loader.load();
 			Stage dialogStage = new Stage();
 	        dialogStage.setTitle("Добавление товара");
+	        dialogStage.getIcons().add(new Image("file:resources/images/goods.png"));
 	        dialogStage.initModality(Modality.WINDOW_MODAL);
 	        Scene scene = new Scene(page);
 	        dialogStage.setScene(scene);
