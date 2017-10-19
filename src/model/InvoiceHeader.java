61609cee2debd9ba02da034040dda206e7d2eff4
MadCat <orloffski@gmail.com>
2017-10-19 04:16:46 +0300

    Rev 0.1.23 Invoice header and line models added

---
+++ src/model/InvoiceHeader.java
@@ -0,0 +1,131 @@
+package model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class InvoiceHeader {
+
+	private final IntegerProperty id;
+	private final StringProperty number;
+	private final StringProperty type;
+	private final StringProperty status;
+	private final StringProperty counterparty;
+	private final IntegerProperty count;
+	private final DoubleProperty summ;
+	private final IntegerProperty counterpartyId;
+	
+	public InvoiceHeader() {
+		this(null, null, null, null, null, null, null, null);
+	}
+	
+	public InvoiceHeader(Integer id, String number, String type, String status, String counterparty, Integer count, Double summ, Integer counterpartyId) {
+		this.id = new SimpleIntegerProperty(id);
+		this.number = new SimpleStringProperty(number);
+		this.type = new SimpleStringProperty(type);
+		this.status = new SimpleStringProperty(status);
+		this.counterparty = new SimpleStringProperty(counterparty);
+		this.count = new SimpleIntegerProperty(count);
+		this.summ = new SimpleDoubleProperty(summ);
+		this.counterpartyId = new SimpleIntegerProperty(counterpartyId);
+	}
+	
+	public int getId() {
+        return id.get();
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+    
+    public int getCount() {
+        return count.get();
+    }
+
+    public void setCount(int count) {
+        this.count.set(count);
+    }
+
+    public IntegerProperty countProperty() {
+        return count;
+    }
+    
+    public int getCounterpartyId() {
+        return counterpartyId.get();
+    }
+
+    public void setCounterpartyId(int counterpartyId) {
+        this.counterpartyId.set(counterpartyId);
+    }
+
+    public IntegerProperty counterpartyIdProperty() {
+        return counterpartyId;
+    }
+    
+    public double getSumm() {
+        return summ.get();
+    }
+
+    public void setSumm(double summ) {
+        this.summ.set(summ);
+    }
+
+    public DoubleProperty summProperty() {
+        return summ;
+    }
+    
+    public String getNumber() {
+        return number.get();
+    }
+
+    public void setNumber(String number) {
+        this.number.set(number);
+    }
+
+    public StringProperty numberProperty() {
+        return number;
+    }
+    
+    public String getType() {
+        return type.get();
+    }
+
+    public void setType(String type) {
+        this.type.set(type);
+    }
+
+    public StringProperty typeProperty() {
+        return type;
+    }
+    
+    public String getStatus() {
+        return status.get();
+    }
+
+    public void setStatus(String status) {
+        this.status.set(status);
+    }
+
+    public StringProperty statusProperty() {
+        return status;
+    }
+    
+    public String getCounterparty() {
+        return status.get();
+    }
+
+    public void setCounterparty(String counterparty) {
+        this.counterparty.set(counterparty);
+    }
+
+    public StringProperty counterpartyProperty() {
+        return counterparty;
+    }
+}
---
+++ src/model/InvoiceLine.java
@@ -0,0 +1,145 @@
+package model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class InvoiceLine {
+
+	private final IntegerProperty id;
+	private final IntegerProperty lineNumber;
+	private final IntegerProperty itemId;
+	private final IntegerProperty vat;
+	private final IntegerProperty extraPrice;
+	private final StringProperty invoiceNumber;
+	private final StringProperty itemName;
+	private final DoubleProperty vendorPrice;
+	private final DoubleProperty retailPrice;
+	
+	public InvoiceLine() {
+		this(null, null, null, null, null, null, null, null, null);
+	}
+	
+	public InvoiceLine(Integer id, Integer lineNumber, String invoiceNumber, Integer itemId, Double vendorPrice, Integer vat, Integer extraPrice, Double retailPrice, String itemName) {
+		this.id = new SimpleIntegerProperty(id);
+		this.lineNumber = new SimpleIntegerProperty(lineNumber);
+		this.itemId = new SimpleIntegerProperty(itemId);
+		this.vat = new SimpleIntegerProperty(vat);
+		this.extraPrice = new SimpleIntegerProperty(extraPrice);
+		this.invoiceNumber = new SimpleStringProperty(invoiceNumber);
+		this.itemName = new SimpleStringProperty(itemName);
+		this.vendorPrice = new SimpleDoubleProperty(vendorPrice);
+		this.retailPrice = new SimpleDoubleProperty(retailPrice);
+	}
+	
+	public int getId() {
+        return id.get();
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+    
+    public int getLineNumber() {
+        return lineNumber.get();
+    }
+
+    public void setLineNumber(int lineNumber) {
+        this.lineNumber.set(lineNumber);
+    }
+
+    public IntegerProperty lineNumberProperty() {
+        return lineNumber;
+    }
+    
+    public int getItemId() {
+        return itemId.get();
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId.set(itemId);
+    }
+
+    public IntegerProperty itemIdProperty() {
+        return itemId;
+    }
+    
+    public int getVat() {
+        return vat.get();
+    }
+
+    public void setVat(int vat) {
+        this.vat.set(vat);
+    }
+
+    public IntegerProperty vatProperty() {
+        return vat;
+    }
+    
+    public int getExtraPrice() {
+        return extraPrice.get();
+    }
+
+    public void setExtraPrice(int extraPrice) {
+        this.extraPrice.set(extraPrice);
+    }
+
+    public IntegerProperty extraPriceProperty() {
+        return extraPrice;
+    }
+    
+    public String getInvoiceNumber() {
+        return invoiceNumber.get();
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber.set(invoiceNumber);
+    }
+
+    public StringProperty invoiceNumberProperty() {
+        return invoiceNumber;
+    }
+    
+    public String getItemName() {
+        return itemName.get();
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName.set(itemName);
+    }
+
+    public StringProperty itemNameProperty() {
+        return itemName;
+    }
+    
+    public double getVendorPrice() {
+        return vendorPrice.get();
+    }
+
+    public void setVendorPrice(double vendorPrice) {
+        this.vendorPrice.set(vendorPrice);
+    }
+
+    public DoubleProperty vendorPriceProperty() {
+        return vendorPrice;
+    }
+    
+    public double getRetailPrice() {
+        return retailPrice.get();
+    }
+
+    public void setRetailPrice(double retailPrice) {
+        this.retailPrice.set(retailPrice);
+    }
+
+    public DoubleProperty retailPriceProperty() {
+        return retailPrice;
+    }
+}
