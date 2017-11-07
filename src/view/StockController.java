03f5955dadb15adb1dd7f61b7029a2a468c8755f
MadCat <orloffski@gmail.com>
2017-11-07 11:00:41 +0300

    Rev 0.2.42 StockView redesigned

--- src/view/StockController.java
+++ src/view/StockController.java
@@ -4,45 +4,22 @@ import java.io.IOException;
 
 import application.Main;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import modes.TabMode;
 import utils.NodeGeneratorUtils;
-import view.stockviews.invoices.InvoicesViewController;
-import view.stockviews.items.ItemsViewController;
 
 public class StockController extends AbstractRootController{
 	
 	private Main main;
-	
+
 	@FXML
-    private BorderPane stockRootLayout;
-	
-	@FXML
-	private ImageView items;
-	
-	@FXML
-	private ImageView invoices;
+	private Text invoicesList;
 	
 	public StockController() {
 	}
 	
 	@FXML
 	private void initialize() {
-		items.setImage(new Image("file:resources/images/goods.png"));
-		invoices.setImage(new Image("file:resources/images/invoices.png"));
-	}
-	
-	@FXML
-	private void openItemsView(){
-		try {
-			NodeGeneratorUtils.openNewTab(rootTab, TabMode.ITEMS_LIST, main);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 	
 	@FXML
@@ -52,6 +29,16 @@ public class StockController extends AbstractRootController{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@FXML
+	private void setInvoicesTextUnderline(){
+		invoicesList.setUnderline(true);
+	}
+
+	@FXML
+	private void removeInvoicesTextUnderline(){
+		invoicesList.setUnderline(false);
 	}
 	
 	public void setMain(Main main) {
