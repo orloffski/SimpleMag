a8ad0ebcc5452e3e0b696562b3337f57f6a53fda
MadCat <orloffski@gmail.com>
2018-01-05 03:14:52 +0300

    Rev 0.3.01 Add ProductsInStock Entity and model

---
+++ src/entity/ProductsInStockEntity.java
@@ -0,0 +1,130 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products_in_stock", schema = "minimag")
+public class ProductsInStockEntity {
+    private int id;
+    private int itemId;
+    private String itemName;
+    private Double itemsCount;
+    private String invoiceNumber;
+    private String invoiceDate;
+    private int counterpartyId;
+
+    public ProductsInStockEntity() {
+    }
+
+    public ProductsInStockEntity(int id, int item_id, String itemName, Double itemsCount, String invoiceNumber, String invoiceDate, int counterpartyId) {
+        this.id = id;
+        this.itemId = item_id;
+        this.itemName = itemName;
+        this.itemsCount = itemsCount;
+        this.invoiceNumber = invoiceNumber;
+        this.invoiceDate = invoiceDate;
+        this.counterpartyId = counterpartyId;
+    }
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "item_id", nullable = false)
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    @Basic
+    @Column(name = "item_name", nullable = true, length = 45)
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Basic
+    @Column(name = "items_count", nullable = true, precision = 0)
+    public Double getItemsCount() {
+        return itemsCount;
+    }
+
+    public void setItemsCount(Double itemsCount) {
+        this.itemsCount = itemsCount;
+    }
+
+    @Basic
+    @Column(name = "invoice_number", nullable = true, length = 45)
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    @Basic
+    @Column(name = "invoice_date", nullable = true, length = 45)
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    @Basic
+    @Column(name = "counterparty_id", nullable = false)
+    public int getCounterpartyId() {
+        return counterpartyId;
+    }
+
+    public void setCounterpartyId(int counterpartyId) {
+        this.counterpartyId = counterpartyId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductsInStockEntity that = (ProductsInStockEntity) o;
+
+        if (id != that.id) return false;
+        if (itemId != that.itemId) return false;
+        if (counterpartyId != that.counterpartyId) return false;
+        if (itemName != null ? !itemName.equals(that.itemName) : that.itemName != null) return false;
+        if (itemsCount != null ? !itemsCount.equals(that.itemsCount) : that.itemsCount != null) return false;
+        if (invoiceNumber != null ? !invoiceNumber.equals(that.invoiceNumber) : that.invoiceNumber != null)
+            return false;
+        if (invoiceDate != null ? !invoiceDate.equals(that.invoiceDate) : that.invoiceDate != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + itemId;
+        result = 31 * result + counterpartyId;
+        result = 31 * result + (itemName != null ? itemName.hashCode() : 0);
+        result = 31 * result + (itemsCount != null ? itemsCount.hashCode() : 0);
+        result = 31 * result + (invoiceNumber != null ? invoiceNumber.hashCode() : 0);
+        result = 31 * result + (invoiceDate != null ? invoiceDate.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/model/ProductsInStock.java
@@ -0,0 +1,115 @@
+package model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
+public class ProductsInStock {
+
+    private final IntegerProperty id;
+    private final IntegerProperty itemId;
+    private final StringProperty itemName;
+    private final DoubleProperty itemsCount;
+    private final StringProperty invoiceNumber;
+    private final StringProperty invoiceDate;
+    private final IntegerProperty counterpartyId;
+
+
+    public ProductsInStock() {
+        this(null,null,null,null,null,null,null);
+    }
+
+    public ProductsInStock(IntegerProperty id, IntegerProperty itemId, StringProperty itemName, DoubleProperty itemsCount, StringProperty invoiceNumber, StringProperty invoiceDate, IntegerProperty counterpartyId) {
+        this.id = id;
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemsCount = itemsCount;
+        this.invoiceNumber = invoiceNumber;
+        this.invoiceDate = invoiceDate;
+        this.counterpartyId = counterpartyId;
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public int getItemId() {
+        return itemId.get();
+    }
+
+    public IntegerProperty itemIdProperty() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId.set(itemId);
+    }
+
+    public String getItemName() {
+        return itemName.get();
+    }
+
+    public StringProperty itemNameProperty() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName.set(itemName);
+    }
+
+    public double getItemsCount() {
+        return itemsCount.get();
+    }
+
+    public DoubleProperty itemsCountProperty() {
+        return itemsCount;
+    }
+
+    public void setItemsCount(double itemsCount) {
+        this.itemsCount.set(itemsCount);
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber.get();
+    }
+
+    public StringProperty invoiceNumberProperty() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber.set(invoiceNumber);
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate.get();
+    }
+
+    public StringProperty invoiceDateProperty() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate.set(invoiceDate);
+    }
+
+    public int getCounterpartyId() {
+        return counterpartyId.get();
+    }
+
+    public IntegerProperty counterpartyIdProperty() {
+        return counterpartyId;
+    }
+
+    public void setCounterpartyId(int counterpartyId) {
+        this.counterpartyId.set(counterpartyId);
+    }
+}
