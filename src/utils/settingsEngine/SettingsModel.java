078ce03c19fa11c8eb341d086e2f31c24c13fe84
MadCat <orloffski@gmail.com>
2018-09-17 17:59:57 +0300

    Rev 0.3.116 Add options to autobackups database
    Добавлены настройки для автоматического сохранения резервных копий базы данных программы

--- src/utils/settingsEngine/SettingsModel.java
+++ src/utils/settingsEngine/SettingsModel.java
@@ -4,6 +4,12 @@ public class SettingsModel {
     public boolean productsInStockEnabled;
     public boolean invoicesFromStock;
     public boolean sellsFromStock;
+
     public boolean autoVat;
     public boolean autoExtraPrice;
+
+    public boolean autoBackupEnabled;
+    public boolean autoBackupOnStart;
+    public boolean autoBackupOnStop;
+    public String autoBackupPath;
 }
--- src/view/settings/productsInStock/ProductsInStockSettingsController.java
+++ src/view/settings/productsInStock/ProductsInStockSettingsController.java
@@ -1,10 +1,15 @@
 package view.settings.productsInStock;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import utils.settingsEngine.SettingsEngine;
 import utils.settingsEngine.SettingsModel;
 import view.AbstractController;
+
+import java.io.File;
 
 public class ProductsInStockSettingsController extends AbstractController {
 
@@ -24,16 +29,31 @@ public class ProductsInStockSettingsController extends AbstractController {
     private CheckBox autoExtraPrice;
 
     @FXML
+    private CheckBox autoBackupEnabled;
+
+    @FXML
+    private CheckBox autoBackupOnStart;
+
+    @FXML
+    private CheckBox autoBackupOnStop;
+
+    @FXML
+    private Button autoBackupPathButton;
+
+    @FXML
+    private TextField autoBackupPath;
+
+    @FXML
     public void initialize(){
         loadSettings(SettingsEngine.getInstance().getSettings());
-        openSettings(productsInStock.isSelected());
+        openSettings(productsInStock.isSelected(), autoBackupEnabled.isSelected());
     }
 
     @FXML
     private void setProductsInStock(){
         SettingsEngine.getInstance().getSettings().productsInStockEnabled = productsInStock.isSelected();
 
-        openSettings(productsInStock.isSelected());
+        openSettings(productsInStock.isSelected(), autoBackupEnabled.isSelected());
     }
 
     @FXML
@@ -61,21 +81,69 @@ public class ProductsInStockSettingsController extends AbstractController {
 
     }
 
+    @FXML
+    private void setAutoBackupEnabled(){
+        SettingsEngine.getInstance().getSettings().autoBackupEnabled = autoBackupEnabled.isSelected();
+
+        openSettings(productsInStock.isSelected(), autoBackupEnabled.isSelected());
+    }
+
+    @FXML
+    private void setAutoBackupOnStart(){
+        SettingsEngine.getInstance().getSettings().autoBackupOnStart = autoBackupOnStart.isSelected();
+    }
+
+    @FXML
+    private void setAutoBackupOnStop(){
+        SettingsEngine.getInstance().getSettings().autoBackupOnStop = autoBackupOnStop.isSelected();
+    }
+
+    @FXML
+    private void setAutoBackupPath(){
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Выбор пути сохранения резервных копий");
+
+        File file = directoryChooser.showDialog(null);
+        if(file!=null){
+            autoBackupPath.setText(file.getPath());
+        }
+
+        SettingsEngine.getInstance().getSettings().autoBackupPath = autoBackupPath.getText();
+    }
+
     private void loadSettings(SettingsModel settings){
         productsInStock.setSelected(settings.productsInStockEnabled);
         invoiceFromStock.setSelected(settings.invoicesFromStock);
         sellsFromStock.setSelected(settings.sellsFromStock);
+
         autoVat.setSelected(settings.autoVat);
         autoExtraPrice.setSelected(settings.autoExtraPrice);
+
+        autoBackupEnabled.setSelected(settings.autoBackupEnabled);
+        autoBackupOnStart.setSelected(settings.autoBackupOnStart);
+        autoBackupOnStop.setSelected(settings.autoBackupOnStop);
+        autoBackupPath.setText(settings.autoBackupPath);
     }
 
-    private void openSettings(boolean isProductsInStockEnabled){
+    private void openSettings(boolean isProductsInStockEnabled, boolean isAutoBackupEnabled){
         if(isProductsInStockEnabled){
             invoiceFromStock.setDisable(false);
             sellsFromStock.setDisable(false);
         }else{
             invoiceFromStock.setDisable(true);
             sellsFromStock.setDisable(true);
+        }
+
+        if(isAutoBackupEnabled){
+            autoBackupOnStart.setDisable(false);
+            autoBackupOnStop.setDisable(false);
+            autoBackupPath.setEditable(true);
+            autoBackupPathButton.setDisable(false);
+        }else{
+            autoBackupOnStart.setDisable(true);
+            autoBackupOnStop.setDisable(true);
+            autoBackupPath.setEditable(false);
+            autoBackupPathButton.setDisable(true);
         }
     }
 }
